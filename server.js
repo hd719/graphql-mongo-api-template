a7768bf3c04e08c417b83a46faaca49b40528462
Hamel Desai <hameldesai93@gmail.com>
2022-10-08 14:03:33 -0400

    Basic config and query

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./typedefs");
+const resolvers = require("./resolvers");
+
+async function startServer() {
+  const app = express();
+  const apolloServer = new ApolloServer({ typeDefs, resolvers });
+
+  await apolloServer.start(); // recommended to start the apollo server first, before listening to the app on any port
+  apolloServer.applyMiddleware({ app }); // default path is localhost:4000/graphql
+
+  app.use((req, res) => res.send("Hello from express-apollo server"));
+
+  console.log("process.env.MONGO_DB_URL:", process.env.MONGO_DB_URL);
+
+  await mongoose.connect(`${process.env.MONGO_DB_URL}`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+
+  console.log("Mongoose connected! ");
+
+  app.listen(`${process.env.PORT}`, () =>
+    console.log("Server initialized on PORT 4000")
+  );
+}
+
+startServer();
